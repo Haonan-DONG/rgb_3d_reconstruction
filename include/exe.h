f6b1ea5f1ceb91a9216d839f68660378611d53d6
Haonan-DONG <august_dong@163.com>
2022-08-08 21:54:57 +0800

    upload the main function

---
+++ include/exe.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+using hash_t = size_t;
+constexpr hash_t prime = 0x100000001B3ull;
+constexpr hash_t basis = 0xCBF29CE484222325ull;
